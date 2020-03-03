c27d561ca7abe93a7a97d1c9354db142e1ea138d
Olesya <Olesabriling@gmail.com>
2020-03-03 22:09:56 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите ваше имя: ");
+        String name = in.nextLine();
+        System.out.println("Добро пожаловать, "+name);
+    }
+}
